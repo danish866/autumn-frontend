106cce06e8587cbfaddb2eb49441e171fc676a09
Danish Rashed <danishrashed@live.com>
2025-04-04 01:13:50 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Footer from "./components/Footer"
 import Features from "./components/Features"
 function App() {
   return (
-    
-    <>
-    < Navbar />
-    < Features />
-    < Footer />
-    </>
+    <div className="flex flex-col min-h-screen">
+      < Navbar />
+      <main className="flex-1">
+        < Features />
+      </main>
+      < Footer />
+    </div>
   )
 }
 
