b7f3b9944acafae0392b374056c7402758c98e64
Danish Rashed <danishrashed@live.com>
2025-04-25 15:59:03 +0500

    added changes form

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,9 +1,17 @@
 import PropTypes from 'prop-types';
 import {useState} from 'react';
+import { Link } from 'react-router-dom';
+import { validateEmail,validatePassword } from '../utilities/validations';
 
+const intitialErrorState = {
+  email: '',
+  password: '',
+  general: ''
+}
 const Authentication = ({pageType}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setError] = useState(intitialErrorState);
   
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -14,8 +22,20 @@ const Authentication = ({pageType}) => {
   }
 
   const handleSubmit = (e) => {
-    console.log('email', email);
-    console.log('password', password);
+    if (!validateEmail(email)) {
+      setError(prev => ({
+        ...prev,
+        email: 'Please enter a valid email address'
+      }));
+    }
+
+    if (!validatePassword(password)) {
+      setError({
+        ...prev,
+        password: 'Password must be at least 6 characters long'
+      });
+    }
+
     e.preventDefault();
   }
 	return (
@@ -24,23 +44,44 @@ const Authentication = ({pageType}) => {
         <h3 className="text-2xl font-bold">
           {(pageType === PageType.Login) ? 'Login' : 'Register'}
         </h3>
+
+        {(pageType === PageType.Login) ? 
+          <p className="mt-4"> Not a member? 
+            <span className="text-blue-500 ms-1 cursor-pointer">
+              <Link to="/register">Register</Link>         
+            </span>  
+          </p>
+          :
+          <p className="mt-4"> Already a member? 
+            <span className="ms-1 text-blue-500 cursor-pointer">
+              <Link to="/login">Login</Link>
+            </span>  
+          </p>
+        }
         <form onSubmit={handleSubmit} className="mt-10 max-w-96 flex flex-col gap-4">
-          <input
-            name="email"
-            type="email"
-            className="py-2 border border-gray-600 rounded-md px-4"
-            placeholder="Enter Email"
-            onChange={handleEmailChange}
-            value={email}
-          />
-          <input
-            name="password"
-            type="password"
-            className="py-2 border border-gray-600 rounded-md px-4"
-            placeholder="Enter Password"
-            onChange={handlePasswordChange}
-            value={password}
-          />
+        
+          <div>
+            <input
+              name="email"
+              type="email"
+              className="py-2 w-full border border-gray-600 rounded-md px-4"
+              placeholder="Enter Email"
+              onChange={handleEmailChange}
+              value={email}
+            />
+            {errors.email && <p className="text-sm text-medium text-red-500 mt-1">{errors.email}</p>}
+          </div>
+          <div>
+            <input
+              name="password"
+              type="password"
+              className="py-2 w-full border border-gray-600 rounded-md px-4"
+              placeholder="Enter Password"
+              onChange={handlePasswordChange}
+              value={password}
+            />
+            {errors.password && <p className="text-sm text-medium text-red-500 mt-1">{errors.password}</p>}
+          </div>
 
           <button
             type="submit"
