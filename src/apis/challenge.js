53c0d8b958133f515c68544a2fd7339d0e84a0d9
Danish Rashed <danishrashed@live.com>
2025-05-12 11:14:50 +0500

    changes

--- src/apis/challenge.js
+++ src/apis/challenge.js
@@ -1,3 +1,27 @@
+// Add a new challenge
+export const addChallengeApi = async (bodyObject, jwtToken) => {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `${jwtToken}`
+    },
+    body: JSON.stringify(bodyObject),
+  };
+  try {
+    debugger
+    const response = await fetch(`${DOMAIN}/api/v1/challanges`, requestOptions);
+    const result = await response.json();
+    debugger
+    if (response.ok) {
+      return [result, ''];
+    } else {
+      return ['', result.status?.message || 'Unknown server error'];
+    }
+  } catch (error) {
+    return ['', `Server Down: ${error}`];
+  }
+};
 import { DOMAIN } from "./config";
 
 // Fetch all challenges
